24d14a92573ff3050159b10b65ae5b55193034b4
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-08 19:16:16 +0530

    Create Question-03.c

---
+++ Question-03.c
@@ -0,0 +1,30 @@
+// Write a program to calculate the sum of n numbers entered by the user using malloc and free.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+    int *p , n , sum=0 , i ;
+
+    printf("Enter size of array : ") ;
+    scanf("%d",&n) ;
+
+    p = (int*) malloc(n) ;
+
+    if (p==NULL)
+    {
+        printf("memory allocation failed.");
+        return 0 ;
+    }
+
+    printf("Enter %d number : ",n) ;
+    for(i=0 ; i<n ; i++)
+        scanf("%d",(p+i)) ;
+
+    for(i=0 ; i<n ; i++)
+        sum = sum + *(p+i) ;
+
+    printf("Sum is %d",sum) ;
+
+}
